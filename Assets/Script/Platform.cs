bcc4cd4e07afc108124286f82e192f38a0f386b6
MehmetEmin <m.eminyagli@gmail.com>
2023-02-19 23:12:01 +0300

    platform mobil hareket eklendi

--- Assets/Script/Platform.cs
+++ Assets/Script/Platform.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     public float speed;
+    public float maxheight;
 
+    private Vector3 targetPosition; //hedef pozisyonu bulmak için
     void Start()
     {
         
@@ -22,9 +24,33 @@ public class Platform : MonoBehaviour
 
     public void Hareket()
     {
+        //mobil için
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                        targetPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                        targetPosition.z = transform.position.z;
+                    break;
+                
+                case TouchPhase.Moved:
+                        targetPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                        targetPosition.z = transform.position.z;
+                    if (targetPosition.y > maxheight)
+                    {
+                        targetPosition.y = maxheight;
+                    }
+                    break;
+            }
+        }
+        // Hareket hesaplama ve uygulama
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        //pc için
         float horizontal = Input.GetAxis("Horizontal");
-
-
         if (Input.GetKey(KeyCode.A))
         {
             Vector3 newposition = transform.position + new Vector3(horizontal * speed * Time.deltaTime, 0, 0);
