fe13a193ec57ba4d5e504f889279c459a218cf46
MehmetEmin <m.eminyagli@gmail.com>
2023-02-21 00:14:38 +0300

    pota collider eklendi top potaya girince sayı artırılması yapıldı

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text skor;
+    private int sayac = 0;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        sayac++;
+        skor.text = sayac.ToString();
+    }
+
     void Start()
     {
         
